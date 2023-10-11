486c4d8d645c3ac9989e3b469031dad1c3c6ffa6
GeT <Artem-klevtsov@mail.ru>
2023-10-12 01:41:56 +0300

    Есть проблема двойного открытия страницы, но вроде работает.

---
+++ App1/App1/SeecondActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App1
+{
+    [Activity(Label = "SeecondActivity")]
+    public class SeecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Create your application here
+            SetContentView(Resource.Layout.second_page);
+            Button sbmButton = FindViewById<Button>(Resource.Id.button1);
+            sbmButton.Click += delegate { StartActivity(typeof(MainActivity)); };
+        }
+    }
+}
